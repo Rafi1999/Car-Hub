0655724466b9d3c5748f64d8067084a377336cdc
Al Rafi <60866655+Rafi1999@users.noreply.github.com>
2023-10-05 22:52:56 +0600

    upto search

--- src/Layout/CarCard.jsx
+++ src/Layout/CarCard.jsx
@@ -1,16 +1,45 @@
-
+import { BsFillPeopleFill,BsHeart} from "react-icons/bs";
+import { FaGasPump } from "react-icons/fa";
+import { MdOutlineAvTimer } from "react-icons/md";
+import { GiSteeringWheel } from "react-icons/gi";
 const CarCard = ({car}) => {
-   const {name,price,description,picture} = car;
+   const {name,price,picture,manufacture_year,seat_capacity,mileage,transmission,fuel_type} = car;
    return (
-      <div className="card w-96 h-96 bg-gray-300 shadow-xl">
-  <figure className="px-10 pt-10">
-    <img src={picture} alt="Car" className="rounded-xl" />
+      <div className="card w-[400px] bg-slate-200 shadow-md">
+  <figure className="px-4 pt-8">
+    <img src={picture} alt="Car" className="rounded-xl h-[270px] object-cover w-screen" />
   </figure>
-  <div className="card-body items-center text-center">
+  <div className="card-body">
+    <div className="flex justify-between items-center">
     <h2 className="card-title">{name}</h2>
-    <p>{price}</p>
-    <div className="card-actions">
-      <button className="btn btn-primary">Buy Now</button>
+    <h2 className="border-2 border-gray-400 rounded-lg p-1">{manufacture_year}</h2>
+    </div>
+    <div className="flex justify-between">
+      <div className="flex gap-2 items-center">
+      <BsFillPeopleFill className="text-cyan-600"></BsFillPeopleFill>
+      <p className="text-gray-600">{seat_capacity}</p>
+      </div>
+      <div className="flex items-center gap-2">
+      <FaGasPump className="text-cyan-600"></FaGasPump>
+      <p className="text-gray-600">{fuel_type}</p>
+      </div>
+    </div>
+    <div className="flex justify-between">
+      <div className="flex gap-2 items-center">
+      <MdOutlineAvTimer className="text-cyan-600"></MdOutlineAvTimer>
+      {fuel_type!= 'Electric'? <p className="text-gray-600">{mileage} Km/1-litre</p>: <p className="text-gray-600">{mileage} Km/1-charge</p>}
+      </div>
+      <div className="flex items-center gap-2">
+      <GiSteeringWheel className="text-cyan-600"></GiSteeringWheel>
+      <p className="text-gray-600">{transmission}</p>
+      </div>
+    </div>
+        <div className="flex justify-between border-t-2 border-t-gray-300 mt-3 items-center">
+          <p className="mt-3 text-2xl font-semibold">${price}</p>
+          <div className="flex items-center self-center gap-2 mt-3">
+          <button><BsHeart className="text-cyan-600"></BsHeart></button>
+          <button className="btn-info rounded-2xl btn-sm font-medium">Buy</button>
+          </div>
     </div>
   </div>
 </div>
--- src/Layout/Home.jsx
+++ src/Layout/Home.jsx
@@ -1,10 +1,13 @@
-import { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import Search from "./Search";
 import Navbar from "./Navbar";
 import CarCard from "./CarCard";
 
 const Home = () => {
   const [cars, setCars] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchQuery, setSearchQuery] = useState(""); // State for search query
+  const carsPerPage = 6;
 
   useEffect(() => {
     fetch("../../cars.json")
@@ -13,14 +16,53 @@ const Home = () => {
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
+  // Calculate the index range for the current page
+  const indexOfLastCar = currentPage * carsPerPage;
+  const indexOfFirstCar = indexOfLastCar - carsPerPage;
+
+  // Filter cars based on the search query
+  const filteredCars = cars.filter((car) =>
+    car.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const currentCars = filteredCars.slice(indexOfFirstCar, indexOfLastCar);
+
+  // Function to handle page change
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
+
+  // Function to handle search input change
+  const handleSearchInputChange = (event) => {
+    setSearchQuery(event.target.value);
+    setCurrentPage(1); // Reset to the first page when searching
+  };
+
   return (
     <div>
       <Navbar />
-      <Search />
-      <div className="grid grid-cols-3 gap-2 items-center justify-center self-center">
-        {cars.map((each) => (
+      <Search onChange={handleSearchInputChange} value={searchQuery} />
+      <div className="grid sm:grid-cols-1 md:grid-cols-3 justify-items-center gap-5 my-14">
+        {currentCars.map((each) => (
           <CarCard key={each.id} car={each} />
         ))}
+      </div>
+      <div className="flex justify-center mt-4">
+        {Array.from({ length: Math.ceil(filteredCars.length / carsPerPage) }).map(
+          (page, index) => (
+            <button
+              key={index}
+              className={`px-4 py-2 mx-2 mb-8 rounded-full ${
+                currentPage === index + 1
+                  ? "bg-blue-500 text-white"
+                  : "bg-gray-200 hover:bg-gray-300"
+              }`}
+              onClick={() => handlePageChange(index + 1)}
+            >
+              {index + 1}
+            </button>
+          )
+        )}
       </div>
     </div>
   );
